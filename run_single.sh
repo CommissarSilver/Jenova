cfc44985f4fd7cfe1d98e20ac563a79b6395d98c
CommissarSilver <a.majdinasab@hotmail.com>
2022-03-24 11:05:37 -0400

    updated bash file

--- run_single.sh
+++ run_single.sh
@@ -1,11 +1,16 @@
-!/bin/bash
+#!/bin/bash
 #SBATCH --job-name=mpi_job
+#SBATCH --account=def-foutsekh
+#SBATCH --time=24:00:00
+#SBATCH --mail-user=commissarsilver@gmail.com
+#SBATCH --mail-type=ALL
+
 
 module load NiaEnv/2019b python/3.8
-mkdir ~/.virtualenvs
-cd ~/.virtualenvs
-virtualenv --system-site-packages ~/.virtualenvs/tprl
-source ~/.virtualenvs/tprl/bin/activate
+mkdir /scratch/f/foutsekh/nikanjam
+cd /scratch/f/foutsekh/nikanjam
+virtualenv --system-site-packages /scratch/f/foutsekh/nikanjam/tprl
+source /scratch/f/foutsekh/nikanjam/tprl/bin/activate
 
 git clone https://github.com/CommissarSilver/Jenova
 cd Jenova
