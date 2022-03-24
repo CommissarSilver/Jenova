fb07fd28464fd6169e3c904bde4b6ff4bfc96bb2
CommissarSilver <a.majdinasab@hotmail.com>
2022-03-24 13:51:05 -0400

    update

--- run_single.sh
+++ run_single.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=mpi_job
+#SBATCH --job-name=jenova1
 #SBATCH --account=def-foutsekh
 #SBATCH --time=24:00:00
 #SBATCH --mail-user=commissarsilver@gmail.com
@@ -7,12 +7,11 @@
 
 
 module load NiaEnv/2019b python/3.8
-mkdir /scratch/f/foutsekh/nikanjam
-cd /scratch/f/foutsekh/nikanjam
+mkdir /scratch/f/foutsekh/nikanjam/tprl
+cd /scratch/f/foutsekh/nikanjam/tprl
 virtualenv --system-site-packages /scratch/f/foutsekh/nikanjam/tprl
 source /scratch/f/foutsekh/nikanjam/tprl/bin/activate
 
-git clone https://github.com/CommissarSilver/Jenova
-cd Jenova
+cd /scratch/f/foutsekh/nikanjam/Jenova
 pip install -r requirements.txt
 python tpdrl.py
